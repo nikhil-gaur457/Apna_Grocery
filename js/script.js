9ca62995ddfff35377f0eff8f488916ab2c409a0
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2022-05-09 23:25:09 +0530

    adding window swiper for product section

--- js/script.js
+++ js/script.js
@@ -32,3 +32,24 @@ document.querySelector('#menu-btn').onclick = () => {
     shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
 }
+
+var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
