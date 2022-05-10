427c2b44e6838d102fbda1a548abfd72ee7790eb
Shubham Shukla <67850120+shubhamshukla12@users.noreply.github.com>
2022-05-10 23:53:46 +0530

    connection with database

--- BACKEND/src/db/conn.js
+++ BACKEND/src/db/conn.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/Apna_Grocery",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex : true
+}).then(() => {
+    console.log('connenction successful');
+}).catch((e) => {
+    console.log('No Connection');
+})
